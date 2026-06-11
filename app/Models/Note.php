c14a2c9966136f4b93b333f8ae3f47f18216d5ed
Finnley <per.finnleygrange@icloud.com>
2026-06-12 00:40:22 +0100

    Add order_number, message, and author to notes table and model

--- app/Models/Note.php
+++ app/Models/Note.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Note extends Model
 {
-    //
+    protected $fillable = [
+        'order_number',
+        'message',
+        'author'
+    ];
 }
--- database/migrations/2026_06_11_233130_create_notes_table.php
+++ database/migrations/2026_06_11_233130_create_notes_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('notes', function (Blueprint $table) {
             $table->id();
+            $table->string('order_number');
+            $table->text('message');
+            $table->string('author');
             $table->timestamps();
         });
     }
