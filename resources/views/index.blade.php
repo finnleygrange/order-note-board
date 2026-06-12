6f4f7bb7340ced1baa88127c1ac95ee8788b3c52
Finnley <per.finnleygrange@icloud.com>
2026-06-12 19:37:51 +0100

    Enhance index view layout with Bootstrap styling and improve notes display

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,31 +4,39 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
     <title>Order Note Board</title>
 </head>
 <body>
-    <div id="app">
-        <h2>Order Note Board</h2>
+    <div id="app" class="container-fluid my-4">
+        <h2 class="mb-4">Order Note Board</h2>
         
-        <form @submit.prevent="save">
-            <label for="order_number">Order Number</label><br>
-            <input type="text" v-model="form.order_number" required><br>
+        <form @submit.prevent="save" class="row row-cols-lg-auto g-3 align-items-center mb-5">
+            <div class="col-12">
+                <input type="text" class="form-control" v-model="form.order_number" placeholder="Order Number" required>
+            </div>
 
-            <label for="message">Message</label><br>
-            <input type="text" v-model="form.message" required><br>
+            <div class="col-12">
+                <input type="text" class="form-control" v-model="form.message" placeholder="Message" required>
+            </div>
 
-            <label for="author">Author</label><br>
-            <input type="text" v-model="form.author" required><br><br>
+            <div class="col-12">
+                <input type="text" class="form-control" v-model="form.author" placeholder="Author" required>
+            </div>
 
-            <button type="submit">Save Note</button>
+            <div class="col-12">
+                <button type="submit" class="btn btn-primary">Save Note</button>
+            </div>
         </form>
 
-        <div id="notes">
-            <h3>Notes:</h3>
-            <p v-for="n in notes">
-                Order: @{{n.order_number}} - @{{n.message}} (@{{n.author}})
-            </p>
+        <div id="notes" class="pt-3 border-top">
+            <h3 class="h5 mb-3">Notes:</h3>
+            <ul class="list-group">
+                <li v-for="n in notes" class="list-group-item">
+                    Order: @{{n.order_number}} - @{{n.message}} (@{{n.author}})
+                </li>
+            </ul>
         </div>
     </div>
 
